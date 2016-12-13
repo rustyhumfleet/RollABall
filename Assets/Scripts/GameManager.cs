af54b49b9a53bf80fd0e2bcb1aca8219667d8255
Rusty Humfleet <rustyhumfleet@gmail.com>
2016-12-12 19:42:08 -0500

    2nd commit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -25,7 +25,7 @@ public class GameManager : MonoBehaviour
             //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
             Destroy(gameObject);
         }
-         
+        
         //Sets this to not be destroyed when reloading scene
         DontDestroyOnLoad(gameObject);
         //Get a component reference to the attached LevelManager script
